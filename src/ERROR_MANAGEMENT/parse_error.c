23dff93fda2742e5221b6addd1f9b7ce9090da28
Hp de moa <taquettom3@gmail.com>
2025-04-10 18:00:42 +0200

    + | Created main and parse error and start coding :)

---
+++ src/ERROR_MANAGEMENT/parse_error.c
@@ -0,0 +1,17 @@
+#include "Cube3d.h"
+
+int arg_error(int error_flag)
+{
+	if (error_flag == TOO_FEW_ARGUMENT)
+		ft_putstr_fd("Too few argument.\nUsage: ./Cube3d <test>.cub\n",
+			STDERR_FILENO);
+	else if (error_flag == TOO_MANY_ARGUMENT)
+		ft_putstr_fd("Too many argument.\nUsage: ./Cube3d <test>.cub\n",
+			STDERR_FILENO);
+	else if (error_flag == INVALID_FILE_FORMAT)
+		ft_putstr_fd("Invalid file format.\nThe file format must be \".cub\"\n",
+			STDERR_FILENO);
+	else if (error_flag == INVALID_FILE)
+		ft_putstr_fd("The File don't exist or can't be open\n", STDERR_FILENO);
+	return (EXIT_SUCCESS);
+}
