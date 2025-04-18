8c40a5153f1dcc04e70b2782aa9b12b3b35134fe
Hp de moa <taquettom3@gmail.com>
2025-04-18 18:05:52 +0200

    ~ | Updated macro error

--- Include/error_management.h
+++ Include/error_management.h
@@ -8,12 +8,14 @@
 # define INT_INVALID_FILE_FORMAT 3
 # define INT_INVALID_FILE 4
 # define INT_MAP_INVALID_PARAM 5
+# define INT_MAP_INVALID_CHAR 6
 
 # define STR_TOO_FEW_ARGUMENT "Too few argument.\nUsage: ./Cube3d <test>.cub\n"
 # define STR_TOO_MANY_ARGUMENT "Too many argument.\nUsage: ./Cube3d <test>.cub\n"
 # define STR_INVALID_FILE_FORMAT "Invalid file format.\nThe file format must be \".cub\"\n"
 # define STR_INVALID_FILE "The File don't exist or can't be open\n"
 # define STR_MAP_INVALID_PARAM "The file contains an invalid parameter\n"
+# define STR_MAP_INVALID_CHAR "The map contains an invalid character\n"
 
 int		arg_error(int error_flag);
 bool	parse_error(int error_flag);
