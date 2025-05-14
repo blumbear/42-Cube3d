cb236eafd842eddc5a4f1473e60d7d1abe79a8c4
Hp de moa <taquettom3@gmail.com>
2025-05-14 14:27:41 +0200

    ~+|  Updated error message and Added more error message

--- Include/error_management.h
+++ Include/error_management.h
@@ -9,6 +9,8 @@
 # define INT_INVALID_FILE 4
 # define INT_MAP_INVALID_PARAM 5
 # define INT_MAP_INVALID_CHAR 6
+# define INT_DOUBLE_PLAYER_IN_MAP 7
+# define INT_MAP_IS_NOT_SURROUNDED 8
 
 # define STR_TOO_FEW_ARGUMENT "Too few argument.\nUsage: ./Cube3d <test>.cub\n"
 # define STR_TOO_MANY_ARGUMENT "Too many argument.\nUsage: ./Cube3d <test>.cub\n"
@@ -16,6 +18,12 @@
 # define STR_INVALID_FILE "The File don't exist or can't be open\n"
 # define STR_MAP_INVALID_PARAM "The file contains an invalid parameter\n"
 # define STR_MAP_INVALID_CHAR "The map contains an invalid character\n"
+# define STR_DOUBLE_PLAYER_IN_MAP "The map contain two or more players\n"
+# define STR_MAP_IS_NOT_SURROUNDED "The area around the player is not fully enclosed by walls.\n"
+
+
+# define PARSE_ERROR_MSG "PARSE_ERROR: "
+# define ARGUMENT_ERROR_MSG "ARGUMENTS_ERROR: "
 
 int		arg_error(int error_flag);
 bool	parse_error(int error_flag);
--- src/ERROR_MANAGEMENT/error_handler.c
+++ src/ERROR_MANAGEMENT/error_handler.c
@@ -2,17 +2,23 @@
 
 bool parse_error(int error_flag)
 {
+	ft_putstr_fd(PARSE_ERROR_MSG, STDERR_FILENO);
 	if (error_flag == INT_MAP_INVALID_PARAM)
 		ft_putstr_fd(STR_MAP_INVALID_PARAM, STDERR_FILENO);
-	if (error_flag == INT_MAP_INVALID_PARAM)
+	else if (error_flag == INT_MAP_INVALID_PARAM)
 		ft_putstr_fd(STR_MAP_INVALID_PARAM, STDERR_FILENO);
-	if (error_flag == INT_MAP_INVALID_CHAR)
+	else if (error_flag == INT_MAP_INVALID_CHAR)
 		ft_putstr_fd(STR_MAP_INVALID_CHAR, STDERR_FILENO);
+	else if (error_flag == INT_DOUBLE_PLAYER_IN_MAP)
+		ft_putstr_fd(STR_DOUBLE_PLAYER_IN_MAP, STDERR_FILENO);
+	else if (INT_MAP_IS_NOT_SURROUNDED)
+		ft_putstr_fd(STR_MAP_IS_NOT_SURROUNDED, STDERR_FILENO);
 	return (false);
 }
 
 int arg_error(int error_flag)
 {
+	ft_putstr_fd(ARGUMENT_ERROR_MSG, STDERR_FILENO);
 	if (error_flag == INT_TOO_FEW_ARGUMENT)
 		ft_putstr_fd(STR_TOO_FEW_ARGUMENT, STDERR_FILENO);
 	else if (error_flag == INT_TOO_MANY_ARGUMENT)
