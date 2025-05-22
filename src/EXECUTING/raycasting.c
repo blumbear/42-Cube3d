6a5856ca83709d68c3c0556234c75c761d08c659
Saiban0 <bartched@outlook.fr>
2025-05-22 19:35:28 +0200

    + | First raycasting iteration (absolutely not working I'm just tired)

---
+++ src/EXECUTING/raycasting.c
@@ -0,0 +1,57 @@
+#include "Cube3d.h"
+
+void	draw_rays(t_env *env)
+{
+	int		i;
+	int		dof;
+	int		map_x;
+	int		map_y;
+	t_coord	ray_coords;
+	float	x_offset;
+	float	y_offset;
+	float	tan_inverse;
+
+	i = 0;
+	ray_coords.pos_x = 0;
+	ray_coords.pos_y = 0;
+	ray_coords.angle = env->player_coord->angle;
+	while (i < 1)
+	{
+		dof = 0;
+		tan_inverse = -1/tan(ray_coords.angle);
+		if (ray_coords.angle > PI) //south
+		{
+			ray_coords.pos_y = (((int)ray_coords.pos_y >> 6) << 6 ) - 0.0001;
+			ray_coords.pos_x = (env->player_coord->pos_x - ray_coords.pos_x) * tan_inverse + env->player_coord->pos_x;
+			y_offset = -64;
+			x_offset = -y_offset * tan_inverse;
+		}
+		else if (ray_coords.angle < PI) //north
+		{
+			ray_coords.pos_y = (((int)ray_coords.pos_y >> 6) << 6 ) + 64;
+			ray_coords.pos_x = (env->player_coord->pos_x - ray_coords.pos_x) * tan_inverse + env->player_coord->pos_x;
+			y_offset = 64;
+			x_offset = -y_offset * tan_inverse;
+		}
+		else if (ray_coords.angle == 0 || ray_coords.angle == PI) //east or west
+		{
+			ray_coords.pos_x = env->player_coord->pos_x;
+			ray_coords.pos_y = env->player_coord->pos_y;
+			dof = DEPTH_OF_FIELD;
+		}
+		while (dof < DEPTH_OF_FIELD)
+		{
+			map_x = (int) ray_coords.pos_x >> 6;	//SHIT
+			map_y = (int) ray_coords.pos_y >> 6;	//SHIT
+			if ((map_x <= env->map_width && map_y <= env->map_height) && env->map[map_x][map_y] == '1')
+				dof = DEPTH_OF_FIELD;
+			else
+			{
+				ray_coords.pos_x += x_offset;
+				ray_coords.pos_y += y_offset;
+				dof++;
+			}
+		}
+		i++;
+	}
+}
