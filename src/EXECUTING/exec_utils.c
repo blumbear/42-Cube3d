55e3cc96271e1a06d7568d9a8e4ac857061f0275
Saiban0 <bartched@outlook.fr>
2025-05-22 19:31:53 +0200

    + | Created 2 functions to get map dimensions

--- src/EXECUTING/exec_utils.c
+++ src/EXECUTING/exec_utils.c
@@ -6,5 +6,28 @@ void	print_player_coords(t_coord *coords)
 	printf("player y = %f\n", coords->pos_y);
 	printf("delta x = %f\n", coords->delta_x);
 	printf("delta y = %f\n", coords->delta_y);
-	printf("player angle = %f\n", coords->player_angle);
+	printf("player angle = %f\n", coords->angle);
+}
+
+int	get_largest_map_line(t_env *env)
+{
+	int	max;
+	int	i;
+
+	max = 0;
+	i = -1;
+	while (env->map_size[++i])
+		if (env->map_size[i] > max)
+			max = env->map_size[i];
+	return (max);
+}
+
+int	get_largest_map_column(t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (env->map_size[i])
+		i++;
+	return (i);
 }
