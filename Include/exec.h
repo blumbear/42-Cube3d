c15073ae5170c8679377c351931df3f5a4da09cc
Saiban0 <bartched@outlook.fr>
2025-05-22 19:30:42 +0200

    + | Added new macros and updated function declarations

--- Include/exec.h
+++ Include/exec.h
@@ -7,6 +7,9 @@
 # define FPS 60
 # define CEILING_COLOR 0x0000FFFF
 # define FLOOR_COLOR 0x7B7B7BFF
+# define ROTATION_SPEED 0.1
+# define MOVEMENT_SPEED 1
+# define DEPTH_OF_FIELD 8
 
 /*			Initialization			*/
 void	exec_init(t_env *env);
@@ -14,13 +17,14 @@ void	exec_init(t_env *env);
 /*			Rendering			*/
 void	render_frame(t_env *frame);
 void	draw_background(t_env *env);
+void	draw_rays(t_env *env);
 
 /*			Looping			*/
 void	main_loop(void	*param);
 
 /*			Utils			*/
 void	print_player_coords(t_coord *coords);
-
-
+int		get_largest_map_line(t_env *env);
+int		get_largest_map_column(t_env *env);
 
 #endif
