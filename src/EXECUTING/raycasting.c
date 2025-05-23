ce001e17cb7934886d7511a0c5994ae309eda0a4
Saiban0 <bartched@outlook.fr>
2025-05-23 16:44:07 +0200

    + | Raycasting now handles 360 angle and draws vertical line

--- src/EXECUTING/raycasting.c
+++ src/EXECUTING/raycasting.c
@@ -10,13 +10,15 @@ void	draw_rays(t_env *env)
 	float	x_offset;
 	float	y_offset;
 	float	tan_inverse;
+	float	tan_negative;
 
 	i = 0;
-	ray_coords.pos_x = 0;
-	ray_coords.pos_y = 0;
+	ray_coords.pos_x = env->player_coord->pos_x;
+	ray_coords.pos_y = env->player_coord->pos_y;
 	ray_coords.angle = env->player_coord->angle;
 	while (i < 1)
 	{
+		//horizontal
 		dof = 0;
 		tan_inverse = -1/tan(ray_coords.angle);
 		if (ray_coords.angle > PI) //south
@@ -28,7 +30,7 @@ void	draw_rays(t_env *env)
 		}
 		else if (ray_coords.angle < PI) //north
 		{
-			ray_coords.pos_y = (((int)ray_coords.pos_y >> 6) << 6 ) + 64;
+			ray_coords.pos_y = (((int)ray_coords.pos_y >> 6) << 6) + 64;
 			ray_coords.pos_x = (env->player_coord->pos_x - ray_coords.pos_x) * tan_inverse + env->player_coord->pos_x;
 			y_offset = 64;
 			x_offset = -y_offset * tan_inverse;
@@ -41,15 +43,57 @@ void	draw_rays(t_env *env)
 		}
 		while (dof < DEPTH_OF_FIELD)
 		{
-			map_x = (int) ray_coords.pos_x >> 6;	//SHIT
-			map_y = (int) ray_coords.pos_y >> 6;	//SHIT
-			if ((map_x <= env->map_width && map_y <= env->map_height) && env->map[map_x][map_y] == '1')
-				dof = DEPTH_OF_FIELD;
-			else
+			map_x = (int) ray_coords.pos_x >> 6;
+			map_y = (int) ray_coords.pos_y >> 6;
+			if (map_x >= 0 && map_y >= 0 && map_x < env->map_width && map_y < env->map_height)
 			{
-				ray_coords.pos_x += x_offset;
-				ray_coords.pos_y += y_offset;
-				dof++;
+				if (env->map[map_x][map_y] == '1')
+					dof = DEPTH_OF_FIELD;
+				else
+				{
+					ray_coords.pos_x += x_offset;
+					ray_coords.pos_y += y_offset;
+					dof++;
+				}
+			}
+		}
+		//vertical
+		dof = 0;
+		tan_negative = -tan(ray_coords.angle);
+		if (ray_coords.angle > PI / 2 && ray_coords.angle < (3 * PI) / 2) //West
+		{
+			ray_coords.pos_x = (((int)ray_coords.pos_y >> 6) << 6 ) - 0.0001;
+			ray_coords.pos_y = (env->player_coord->pos_y - ray_coords.pos_y) * tan_negative + env->player_coord->pos_y;
+			x_offset = -64;
+			y_offset = -x_offset * tan_negative;
+		}
+		else if (ray_coords.angle < PI / 2 || ray_coords.angle > (3 * PI) / 2) //East
+		{
+			ray_coords.pos_x = (((int)ray_coords.pos_x >> 6) << 6) + 64;
+			ray_coords.pos_y = (env->player_coord->pos_y - ray_coords.pos_y) * tan_negative + env->player_coord->pos_y;
+			x_offset = 64;
+			y_offset = -x_offset * tan_negative;
+		}
+		else if (ray_coords.angle == 0 || ray_coords.angle == PI) //North or South
+		{
+			ray_coords.pos_x = env->player_coord->pos_x;
+			ray_coords.pos_y = env->player_coord->pos_y;
+			dof = DEPTH_OF_FIELD;
+		}
+		while (dof < DEPTH_OF_FIELD)
+		{
+			map_x = (int) ray_coords.pos_x >> 6;
+			map_y = (int) ray_coords.pos_y >> 6;
+			if (map_x >= 0 && map_y >= 0 && map_x < env->map_width && map_y < env->map_height)
+			{
+				if (env->map[map_x][map_y] == '1')
+					dof = DEPTH_OF_FIELD;
+				else
+				{
+					ray_coords.pos_x += x_offset;
+					ray_coords.pos_y += y_offset;
+					dof++;
+				}
 			}
 		}
 		i++;
