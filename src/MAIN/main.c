f75a91589b3f1e2ffd70e90ac589340c044b2ae3
Hp de moa <taquettom3@gmail.com>
2025-05-29 23:30:25 +0200

    ~ | Updated MAIN to merge

--- src/MAIN/main.c
+++ src/MAIN/main.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 17:15:06 by tom               #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/05/29 23:14:02 by tom              ###   ########.fr       */
-=======
-/*   Updated: 2025/05/28 18:36:04 by bchedru          ###   ########.fr       */
->>>>>>> 130d7e4 (Bartime (#7))
+/*   Updated: 2025/05/29 23:29:52 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,34 +43,8 @@ void	init_env(t_env *env)
 
 int	main (int ac, char **av)
 {
-<<<<<<< HEAD
-	if (env->NO_image)
-		free(env->NO_image);
-	if (env->SO_image)
-		free(env->SO_image);
-	if (env->WE_image)
-		free(env->WE_image);
-	if (env->EA_image)
-		free(env->EA_image);
-	if (env->C_color)
-		free(env->C_color);
-	if (env->F_color)
-		free(env->F_color);
-	if (env->map)
-		ft_free_double_array(env->map);
-	if (env->map_size)
-		free(env->map_size);
-	if (env->player_coord)
-		free(env->player_coord);
-	return (EXIT_SUCCESS);
-}
-
-int main(int ac, char **av)
-{
-	t_env env;
-=======
 	t_env	env;
->>>>>>> 130d7e4 (Bartime (#7))
+
 	if (ac < 2)
 		return (arg_error(INT_TOO_FEW_ARGUMENT));
 	else if (ac > 2)
@@ -85,15 +55,9 @@ int main(int ac, char **av)
 	if (parse(av[1], &env, false) == false)
 		clean_exit(&env);
 	set_map_size(&env);
-<<<<<<< HEAD
 	if (map_check(&env) == false)
 		clean_exit(&env);
 	exec_init(&env);
-=======
-	exec_init(&env);
-	if (map_check(&env) == false)
-		clean_exit(&env);
->>>>>>> 130d7e4 (Bartime (#7))
 	ft_print_double_array(env.map, 0);
 	mlx_loop(env.mlx);
 	clean_exit(&env);
