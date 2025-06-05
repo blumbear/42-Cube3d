8403e5b75c086bf5ded93ec257c0b63d3291ed0d
Saiban0 <bartched@outlook.fr>
2025-06-05 13:36:01 +0200

    ~ | Modified movement and rotation functions to work with new struct

--- src/EXECUTING/game_loop.c
+++ src/EXECUTING/game_loop.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   game_loop.c                                        :+:      :+:    :+:   */
+/*   env_loop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 23:27:38 by tom               #+#    #+#             */
-/*   Updated: 2025/05/30 16:33:12 by bchedru          ###   ########.fr       */
+/*   Updated: 2025/06/05 12:23:17 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,70 @@
 
 void	move_forward(t_env *env)
 {
-	t_coord	*coord;
-
-	coord = env->player_coord;
-	coord->pos_x += coord->delta_x * MOVEMENT_SPEED;
-	coord->pos_y += coord->delta_y * MOVEMENT_SPEED;
+	if (env->map[(int)(env->player_coord->pos_y)][(int)(env->player_coord->pos_x
+			+ env->player_coord->dir_x * (MOVEMENT_SPEED + 0.1))] != '1')
+		env->player_coord->pos_x += env->player_coord->dir_x * MOVEMENT_SPEED;
+	if (env->map[(int)(env->player_coord->pos_y + env->player_coord->dir_y
+			* (MOVEMENT_SPEED + 0.1))][(int)(env->player_coord->pos_x)] != '1')
+		env->player_coord->pos_y += env->player_coord->dir_y * MOVEMENT_SPEED;
 }
 
 void	move_backward(t_env *env)
 {
-	t_coord	*coord;
-
-	coord = env->player_coord;
-	coord->pos_x -= coord->delta_x * MOVEMENT_SPEED;
-	coord->pos_y -= coord->delta_y * MOVEMENT_SPEED;
+	if (env->map[(int)(env->player_coord->pos_y)][(int)(env->player_coord->pos_x
+			- env->player_coord->dir_x * (MOVEMENT_SPEED + 0.1))] != '1')
+		env->player_coord->pos_x -= env->player_coord->dir_x * MOVEMENT_SPEED;
+	if (env->map[(int)(env->player_coord->pos_y - env->player_coord->dir_y
+			* (MOVEMENT_SPEED + 0.1))][(int)(env->player_coord->pos_x)] != '1')
+		env->player_coord->pos_y -= env->player_coord->dir_y * MOVEMENT_SPEED;
 }
 
 void	move_left(t_env *env)
 {
-	t_coord	*coord;
 	double	new_x;
 	double	new_y;
 
-	coord = env->player_coord;
-	new_x = coord->pos_y;
-	new_y = -coord->pos_x;
-	coord->pos_x += coord->delta_x * MOVEMENT_SPEED;
-	coord->pos_y += coord->delta_y * MOVEMENT_SPEED;
+	new_x = env->player_coord->dir_y;
+	new_y = -env->player_coord->dir_x;
+	if (env->map[(int)(env->player_coord->pos_y)][(int)(env->player_coord->pos_x
+			+ new_x * (MOVEMENT_SPEED + 0.1))] != '1')
+		env->player_coord->pos_x += new_x * MOVEMENT_SPEED;
+	if (env->map[(int)(env->player_coord->pos_y + new_y
+			* (MOVEMENT_SPEED + 0.1))][(int)(env->player_coord->pos_x)] != '1')
+		env->player_coord->pos_y += new_y * MOVEMENT_SPEED;
+
 }
 
 void	move_right(t_env *env)
 {
-	t_coord	*coord;
 	double	new_x;
 	double	new_y;
 
-	coord = env->player_coord;
-	new_x = -coord->pos_y;
-	new_y = coord->pos_x;
-	coord->pos_x += new_x * MOVEMENT_SPEED;
-	coord->pos_y += new_y * MOVEMENT_SPEED;
+	new_x = -env->player_coord->dir_y;
+	new_y = env->player_coord->dir_x;
+	if (env->map[(int)(env->player_coord->pos_y)][(int)(env->player_coord->pos_x
+			+ new_x * (MOVEMENT_SPEED + 0.1))] != '1')
+		env->player_coord->pos_x += new_x * MOVEMENT_SPEED;
+	if (env->map[(int)(env->player_coord->pos_y + new_y
+			* (MOVEMENT_SPEED + 0.1))][(int)(env->player_coord->pos_x)] != '1')
+		env->player_coord->pos_y += new_y * MOVEMENT_SPEED;
 }
 
-void	rotate_camera(t_coord *coord, bool right)
+void	rotate_camera(t_env *env, double angle)
 {
-	if (right)
-	{
-		coord->angle += ROTATION_SPEED;
-		if (coord->angle > 2 * PI)
-			coord->angle -= 2 * PI;
-		coord->delta_x = cos(coord->angle) * 5;
-		coord->delta_y = sin (coord->angle) * 5;
-	}
-	else
-	{
-		coord->angle -= ROTATION_SPEED;
-		if (coord->angle < 0)
-			coord->angle += 2 * PI;
-		coord->delta_x = cos(coord->angle) * 5;
-		coord->delta_y = sin (coord->angle) * 5;
-	}
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = env->player_coord->dir_x;
+	env->player_coord->dir_x = env->player_coord->dir_x * cos(angle)
+		- env->player_coord->dir_y * sin(angle);
+	env->player_coord->dir_y = old_dir_x * sin(angle)
+		+ env->player_coord->dir_y * cos(angle);
+	old_plane_x = env->player_coord->plane_x;
+	env->player_coord->plane_x = env->player_coord->plane_x
+		* cos(angle) - env->player_coord->plane_y * sin(angle);
+	env->player_coord->plane_y = old_plane_x * sin(angle)
+		+ env->player_coord->plane_y * cos(angle);
 }
 
 void	keyhook(mlx_key_data_t keydata, void *param)
@@ -91,9 +95,9 @@ void	keyhook(mlx_key_data_t keydata, void *param)
 	if (mlx_is_key_down(env->mlx, MLX_KEY_D))
 		move_right(env);
 	if (mlx_is_key_down(env->mlx, MLX_KEY_LEFT))
-		rotate_camera(env->player_coord, 0);
+		rotate_camera(env, -ROTATION_SPEED);
 	if (mlx_is_key_down(env->mlx, MLX_KEY_RIGHT))
-		rotate_camera(env->player_coord, 1);
+		rotate_camera(env, ROTATION_SPEED);
 	if (mlx_is_key_down(env->mlx, MLX_KEY_ESCAPE))
 		clean_exit(env);
 }
@@ -109,7 +113,7 @@ void	main_loop(void	*param)
 	current_time = mlx_get_time() * 1000;
 	if (current_time - last_rendering >= 1000 / FPS)
 	{
-		// printf("player x = %f\n player y = %f\n delta x = %f\n delta y = %f\n player angle = %f\n", env->player_coord->pos_x, env->player_coord->pos_y, env->player_coord->delta_x, env->player_coord->delta_y, env->player_coord->angle);
+		// printf("player x = %f\n player y = %f\n", env->player_coord->pos_x, env->player_coord->pos_y);
 		render_frame(env);
 		last_rendering = current_time;
 	}
