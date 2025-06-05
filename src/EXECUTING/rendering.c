2f27f0bd0ba77d2ed474cb3fdedcbcc67dfb5e6a
Saiban0 <bartched@outlook.fr>
2025-06-05 16:25:53 +0200

    + | cleaned up rendering.c

--- src/EXECUTING/rendering.c
+++ src/EXECUTING/rendering.c
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 23:28:06 by tom               #+#    #+#             */
-/*   Updated: 2025/06/05 13:33:20 by bchedru          ###   ########.fr       */
+/*   Updated: 2025/06/05 16:14:23 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	render_frame(t_env *env)
 
 	x = 0;
 	draw_background(env);
-	// draw_rays(env);
 	while (x < WIDTH)
 	{
 		render_wall(env, x);
@@ -52,41 +51,14 @@ void	draw_background(t_env *env)
 	}
 }
 
-void	draw_3d(t_env *env, t_ray ray_coords, float final_dist, int i)
-{
-	float	line_height;
-	int		begin_line;
-	int		end_line;
-	int		x;
-	int		y;
-	(void)ray_coords;
-
-	y = 0;
-	x = (i * 8) + WIDTH;
-	line_height = (CUBE_SIZE * HEIGHT) / final_dist;
-	if (line_height > HEIGHT)
-		line_height = HEIGHT;
-	begin_line = -line_height / 2 + HEIGHT / 2;
-	if (begin_line < 0)
-		begin_line = 0;
-	end_line = line_height / 2 + HEIGHT / 2;
-	if (end_line >= HEIGHT)
-		end_line = HEIGHT - 1;
-	while (y < end_line)
-	{
-		mlx_put_pixel(env->window, x, y, 0x000000FF);
-		y++;
-	}
-}
-
 void	render_wall(t_env *env, int x)
 {
 	int	texture_x;
 	int	wall_height;
 
-	calculate_ray_direction(env, x);
-	init_dda(env);
-	perform_dda(env);
+	set_ray_dir(env, x);
+	raycast_start(env);
+	raycast_loop(env);
 	calculate_wall_distance(env, &wall_height, &texture_x);
 	draw_wall_line(env, x, wall_height, texture_x);
 }
